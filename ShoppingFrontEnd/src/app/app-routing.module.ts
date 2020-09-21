a1935df8f722bb7a83c716545ac1369af7b0e1a0
Quadsqua04 <aryaanshika51@gmail.com>
2020-09-21 16:41:23 +0300

    Login component Added

--- ShoppingFrontEnd/src/app/app-routing.module.ts
+++ ShoppingFrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from'./user/login/login.component';
+import { RegisterComponent } from'./user/register/register.component';
+import { ForgotComponent } from'./user/forgot/forgot.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'login',component: LoginComponent },
+   { path: 'register',component: RegisterComponent },
+   { path: 'forgot',component: ForgotComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ShoppingFrontEnd/src/app/app.module.ts
+++ ShoppingFrontEnd/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ForgotComponent } from './user/forgot/forgot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
