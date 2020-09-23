f5dcdba169ca9bfcd92635a3f9ead1634ec1de68
Quadsqua04 <aryaanshika51@gmail.com>
2020-09-23 19:04:15 +0300

    Login,register,forgot validation done

--- ShoppingFrontEnd/src/app/user/forgot/forgot.component.ts
+++ ShoppingFrontEnd/src/app/user/forgot/forgot.component.ts
@@ -1,4 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, PatternValidator} from '@angular/forms'
+
+
+/**
+ * 
+ * @param form 
+ */
+
+function passwordsMatchValidator(form) {
+  const password = form.get('password')
+  const confirmPassword = form.get('confirmPassword')
+
+  if(password.value !== confirmPassword.value) {
+    confirmPassword.setErrors({ passwordsMatch: true })
+  } else {
+    confirmPassword.setErrors(null)
+  }
+
+  return null
+}
+
+/**
+ * If the data is valid return null else return an object
+ */
+function symbolValidator(control) { //control = registerForm.get('password')
+  if(control.hasError('required')) return null;
+  if(control.hasError('minlength')) return null;
+
+  if(control.value.indexOf('@') > -1) {
+    return null
+  } else {
+    return { symbol: true }
+  }
+}
+ 
 
 @Component({
   selector: 'app-forgot',
@@ -7,9 +42,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotComponent implements OnInit {
 
-  constructor() { }
+  forgotForm: FormGroup;
+  
+  constructor(private builder: FormBuilder){}
 
   ngOnInit() {
+    this.buildForm()
+  }
+
+  buildForm() {
+    this.forgotForm = this.builder.group({
+      otp: ['', [Validators.required, Validators.pattern('[0-9]{6}')]],
+      password: ['', [Validators.required, symbolValidator, Validators.minLength(6)]],
+      confirmPassword: '',
+    }, {
+      validators: passwordsMatchValidator
+    })
+  }
+
+  register() {
+    console.log(this.forgotForm.value)
   }
 
 }
--- ShoppingFrontEnd/src/app/user/register/register.component.ts
+++ ShoppingFrontEnd/src/app/user/register/register.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, PatternValidator} from '@angular/forms'
+
+/**
+ * 
+ * @param form 
+ */
+
+function passwordsMatchValidator(form) {
+  const password = form.get('password')
+  const confirmPassword = form.get('confirmPassword')
+
+  if(password.value !== confirmPassword.value) {
+    confirmPassword.setErrors({ passwordsMatch: true })
+  } else {
+    confirmPassword.setErrors(null)
+  }
+
+  return null
+}
+
+/**
+ * If the data is valid return null else return an object
+ */
+function symbolValidator(control) { //control = registerForm.get('password')
+  if(control.hasError('required')) return null;
+  if(control.hasError('minlength')) return null;
+
+  if(control.value.indexOf('@') > -1) {
+    return null
+  } else {
+    return { symbol: true }
+  }
+}
+ 
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
-
-  constructor() { }
+export class RegisterComponent implements OnInit{
+   
+  registerForm: FormGroup;
+  
+  constructor(private builder: FormBuilder){}
 
   ngOnInit() {
+    this.buildForm()
+  }
+
+  buildForm() {
+    this.registerForm = this.builder.group({
+      yourname: ['', [Validators.required,Validators.pattern('(([A-Z][a-z]*((\s[A-Za-z])?[a-z]*)*))')]],
+      mobileno: ['', [Validators.required, Validators.pattern('([6789][0-9]{9})')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, symbolValidator, Validators.minLength(6)]],
+      confirmPassword: ''
+    }, {
+      validators: passwordsMatchValidator
+    })
+  }
+
+  register() {
+    console.log(this.registerForm.value)
   }
 
 }
