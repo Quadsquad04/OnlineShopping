0438294b27ac285e05c3b0585f592e332a0d2239
Quadsquad <sahilgupta1652@gmail.com>
2020-09-23 22:34:26 +0530

    routing done

--- ShoppingFrontEnd/src/app/app-routing.module.ts
+++ ShoppingFrontEnd/src/app/app-routing.module.ts
@@ -3,14 +3,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from'./user/login/login.component';
 import { RegisterComponent } from'./user/register/register.component';
 import { ForgotComponent } from'./user/forgot/forgot.component';
+import {SigninComponent} from './retailer/signin/signin.component';
+import {ForgetComponent} from './retailer/forget/forget.component';
+import {SignupComponent} from './retailer/signup/signup.component';
 import { CartComponent } from './cart/cart.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { from } from 'rxjs';
 
 const routes: Routes = [
    { path: 'login',component:LoginComponent },
    { path: 'register',component:RegisterComponent },
    { path: 'forgot',component:ForgotComponent },
+   { path: 'signin',component:SigninComponent},
+   { path: 'signup',component:SignupComponent},
+   { path: 'forget',component:ForgetComponent },
    { path: 'cart',component:CartComponent},
    {path: 'wishlist',component:WishlistComponent},
    {path: 'carousel',component:CarouselComponent}
